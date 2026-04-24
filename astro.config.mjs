213c4f78c2fc87f86c255486d9f418f6846b7c39
Niek-Kamer <niekelangelo@gmail.com>
2026-04-24 19:32:14 +0200

    fix wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://zkmcu.dev",
